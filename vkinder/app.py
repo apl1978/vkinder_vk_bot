33d5c5f9ad3432ad15b4f305f5dec7a82106ec34
yi1lei6a3 <x.7machinegun@gmail.com>
2022-08-07 22:43:18 +0300

    add search id

--- vkinder/app.py
+++ vkinder/app.py
@@ -1,6 +1,7 @@
 import requests
 from token_group import token_group
 import vk_api 
+from db.interface_db import add_favourites
 
 vk = vk_api.VkApi(token=token_group)
 
@@ -21,7 +22,37 @@ def request_handler(msg):
 
 #  Ищем id кандита соответствующего параметрам запроса
 def search_candidates(id, token: str, sex: int, status: int, age_from: int, age_to: int, hometown: str) -> list:
-    pass
+    url = URL + "users.search"
+    params = {
+            "access_token": f"{token}",
+            "count": "20",
+            "sex": f"{sex}",
+            "hometown": f"{hometown}",
+            "status": f"{status}",
+            "age_from": f"{age_from}",
+            "age_to": f"{age_to}",
+            "has_photo": "1",
+            "v": "5.132"
+            }
+    try:
+        res = requests.get(url, params=params).json()
+        res = res["response"]["items"]
+    except KeyError:
+        return False 
+    except requests.exceptions.ConnectionError:
+        print( f"Соединение было прервано.")
+    else:
+        if res != []:
+            id_list = []
+            check_list = add_favourites(id)
+            for item in res:
+                if item["id"] in check_list:
+                    return None
+                else:
+                    id_list.append(item["id"])
+            return id_list
+        else:
+            return None
 
 # Получаем список состоящий из адреса домашней стр. и адресов топовых фото
 def get_photo_and_url(id, token) -> list:
